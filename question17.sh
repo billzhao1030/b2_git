dfe324fa1a2d43413092f5d6db90562459aca8f4
billzhao1030 <billzhao1030@gmail.com>
2024-02-03 14:05:55 +1030

    17

--- question17.sh
+++ question17.sh
@@ -1,13 +1,18 @@
+cp dir3/bar dir3/bar_copy
+git add -dir3/bar_copy
+
+git commit -m "add copy"
+
 git checkout -b branch1
 
-mv dir1/dir2/foo dir1/
+git mv dir1/dir2/foo dir1/
 
-rm -r dir1/dir2
-rm dir3/bar_copy
+rmdir dir1/dir2
+git rm dir3/bar_copy
 
 touch newfile1
 
-git add -u 
+git add *
 git commit -m "branch1"
 
 git checkout main
@@ -20,6 +25,6 @@ rm dir1/dir3/*
 
 touch dir1/dir3/newfile2
 
-git add -u 
+git add *
 git commit -m "branch2"
 
