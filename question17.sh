97d04e90b209f0e47d88b6abbafc26ef9c835e98
billzhao1030 <billzhao1030@gmail.com>
2024-02-03 18:09:09 +1030

    17

--- question17.sh
+++ question17.sh
@@ -18,11 +18,13 @@ git commit -m "branch1"
 git checkout -b branch2
 
 mv dir1/dir2/foo dir1/dir2/foo_modified
-mv dir3 dir1
-rm dir1/dir3/* 
+mv dir3/ dir1/
+rm -r dir1/dir3/* 
 
 touch dir1/dir3/newfile2
 
 git add .
 git commit -m "branch2"
+
+git checkout main
 
